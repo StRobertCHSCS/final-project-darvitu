530b102efacc36843b007d7567af66da1127f4dd
daniel-wang04 <daniel.wang221@ycdsbk12.ca>
2020-01-01 21:13:22 -0500

    added tile images

--- blocks.py
+++ blocks.py
@@ -1,15 +1,8 @@
 import arcade
 
-LAVA = arcade.load_texture("lava.png")
 
-arcade.open_window(250, 250, "TEST")
-arcade.start_render()
-
-blocks = [(1, 0, 0, 0, 0), (1, 1, 0, 0, 0), (0, 1, 0, 0, 0), (0, 1, 1, 0, 0), (0, 0, 1, 0, 0)]
-for x in range(len(blocks)):
-    for y in range(len(blocks)):
-        if blocks[x][y] == 0:
-            arcade.draw_texture_rectangle(x * 50 + 25, y * 50 + 25, 50, 50, LAVA)
-
-arcade.finish_render()
-arcade.run()
+class Blocks:
+    LAVA = arcade.load_texture("lava.png")
+    LEAVES = arcade.load_texture("leaves.png")
+    YELLOW_PATH = arcade.load_texture("yellow_path.png")
+    COBBLESTONE = arcade.load_texture("cobblestone.png")
