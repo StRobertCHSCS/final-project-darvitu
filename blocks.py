e6035540c06f97240b118cae52dee7e863e3b6a4
daniel-wang04 <daniel.wang221@ycdsbk12.ca>
2020-01-03 13:14:00 -0500

    added tile images

--- blocks.py
+++ blocks.py
@@ -2,7 +2,10 @@ import arcade
 
 
 class Blocks:
-    LAVA = arcade.load_texture("lava.png")
-    LEAVES = arcade.load_texture("leaves.png")
-    YELLOW_PATH = arcade.load_texture("yellow_path.png")
-    COBBLESTONE = arcade.load_texture("cobblestone.png")
+    LAVA = arcade.load_texture("images/lava.png")
+    LEAVES = arcade.load_texture("images/leaves.png")
+    YELLOW_PATH = arcade.load_texture("images/yellow_path.png")
+    COBBLESTONE = arcade.load_texture("images/cobblestone.png")
+    TREE = arcade.load_texture("images/tree.png")
+
+
