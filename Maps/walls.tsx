0b8bff515168e9247105fcf0c3a11004af9698b2
Anthony3301 <anthony.pirvuti22@ycdsbk12.ca>
2020-01-05 21:20:01 -0500

    work in progress tile map

--- Maps/walls.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.1" name="walls" tilewidth="32" tileheight="32" tilecount="64" columns="8">
- <image source="../images/new wall.png" width="269" height="270"/>
-</tileset>
