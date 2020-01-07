aae0282fd227b000fbfe70a839e88988f2aa0fb1
daniel-wang04 <daniel.wang221@ycdsbk12.ca>
2020-01-06 23:16:06 -0500

    main.py - input of keys complete

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
 import arcade
 from game_window import GameWindow
+
+direction = None
+
+
+def on_update(delta_time):
+    global direction
+    if direction != None:
+        print(direction)
+
+
+def on_key_press(symbol, modifiers):
+    global direction
+    if symbol == arcade.key.RIGHT:
+        direction = "RIGHT"
+    if symbol == arcade.key.LEFT:
+        direction = "LEFT"
+    if symbol == arcade.key.UP:
+        direction = "UP"
+    if symbol == arcade.key.DOWN:
+        direction = "DOWN"
+
+
 def main():
-    arcade.open_window()
+    arcade.open_window(1280, 720, "Main")
+    arcade.start_render()
+    arcade.schedule(on_update, 1 / 60)
     arcade.set_background_color(arcade.color.BLACK)
+    # Override arcade methods
+    window = arcade.get_window()
+    window.on_key_press = on_key_press
+    arcade.finish_render()
     arcade.run()
+
+
+main()
