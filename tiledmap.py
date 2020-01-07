51e2189ecc2bfb0e52bd82c9eed7d26194bbf81b
Anthony3301 <anthony.pirvuti22@ycdsbk12.ca>
2020-01-07 13:14:02 -0500

    test

--- tiledmap.py
+++ tiledmap.py
@@ -6,5 +6,6 @@ class TiledMap(arcade.TiledMap):
 
         map = arcade.tilemap.read_tmx("Maps/test-map-6.tmx")
         self.ground_list = arcade.tilemap.process_layer(map, "Tile Layer 1", 1, "images")
+        self.wall_list = arcade.tilemap.process_layer(map, "walls", 1, "images")
 
 
