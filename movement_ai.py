bca95027391087be1537badeee56ca70bab0b141
daniel-wang04 <daniel.wang221@ycdsbk12.ca>
2020-01-09 23:58:53 -0500

    completed bfs but may use a brute force method instead

--- movement_ai.py
+++ movement_ai.py
@@ -16,8 +16,8 @@ class ShortestPath():
         # create step/distance array
         self.step = [[999999 for x in range(50)] for y in range(50)]
         # create queues
-        queue_x = []
-        queue_x.append(current_x)
+        queue_r = []
+        queue_r.append(current_x)
         queue_y = []
         queue_y.append(current_y)
         # add first node
@@ -25,9 +25,9 @@ class ShortestPath():
         # add end node
         self.step[next_x][next_y] = -1
         # perform breadth-first-search
-        while len(queue_x) is not 0:
+        while len(queue_r) is not 0:
             # current location
-            r = queue_x.pop(0)
+            r = queue_r.pop(0)
             c = queue_y.pop(0)
 
             # move up
@@ -35,22 +35,33 @@ class ShortestPath():
                 if self.map[r - 1][c] is not 1:
                     if self.step[r - 1][c] > self.step[r][c] + 1:
                         self.step[r - 1][c] = self.step[r][c] + 1
-                        queue_x.append(r - 1)
+                        queue_r.append(r - 1)
                         queue_y.append(c)
             # move down
             if r + 1 < 50:
                 if self.map[r + 1][c] is not 1:
                     if self.step[r + 1][c] > self.step[r][c] + 1:
                         self.step[r + 1][c] = self.step[r][c] + 1
-                        queue_x.append(r + 1)
+                        queue_r.append(r + 1)
                         queue_y.append(c)
             # move left
             if c - 1 >= 0:
                 if self.map[r][c - 1] is not 1:
                     if self.step[r][c - 1] > self.step[r][c] + 1:
                         self.step[r][c - 1] = self.step[r][c] + 1
-                        queue_x.append(r)
-                        queue_y.append(c-1)
+                        queue_r.append(r)
+                        queue_y.append(c - 1)
+
+            # move left
+            if c + 1 < 50:
+                if self.map[r][c + 1] is not 1:
+                    if self.step[r][c + 1] > self.step[r][c] + 1:
+                        self.step[r][c + 1] = self.step[r][c] + 1
+                        queue_r.append(r)
+                        queue_y.append(c + 1)
+        # setting direction of enemy
+        count = 1
+
 
 
 ShortestPath().shortest_distance(0, 0, 0, 0)
