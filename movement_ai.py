6507a4075f742020cb43ca1dc3c115f1900961f5
daniel-wang04 <daniel.wang221@ycdsbk12.ca>
2020-01-09 23:41:37 -0500

    starting new movement ai

--- movement_ai.py
+++ movement_ai.py
@@ -1,5 +1,7 @@
 import arcade
 import csv
+import math
+import sys
 
 
 class ShortestPath():
@@ -11,4 +13,23 @@ class ShortestPath():
                 self.map.append(row)
 
     def shortest_distance(self, current_x, current_y, next_x, next_y):
-        pass
+        # create step/distance array
+        step = [[999999 for x in range(50)] for y in range(50)]
+        # create queues
+        queue_x = []
+        queue_x.append(current_x)
+        queue_y = []
+        queue_y.append(current_y)
+        # add first node
+        step[current_x][current_y] = 0
+        # add end node
+        step[next_x][next_y] = -1
+        # perform breadth-first-search
+        while len(queue_x) is not 0:
+            # current location
+            r = queue_x.pop(0)
+            c = queue_y.pop(0)
+
+            #move up
+
+ShortestPath().shortest_distance(0, 0, 0, 0)
