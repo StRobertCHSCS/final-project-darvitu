2b6e8acb08b77da23a5733461e2f0b9d0e6d34f1
Anthony3301 <anthony.pirvuti22@ycdsbk12.ca>
2020-01-13 12:16:38 -0500

    added tutorial world function

--- tiledmap.py
+++ tiledmap.py
@@ -5,7 +5,9 @@ class TiledMap(arcade.TiledMap):
     def __init__(self):
         super().__init__()
 
-        map = arcade.tilemap.read_tmx("Maps/test-map-6.tmx")
-        self.ground_list = arcade.tilemap.process_layer(map, "Tile Layer 1", 1, "images")
+    def tutorial_world():
+        map = arcade.tilemap.read_tmx("tutorial-world.tmx")
+        self.ground_list = arcade.tilemap.process_layer(map, "ground", 1, "images")
         self.wall_list = arcade.tilemap.process_layer(map, "walls", 1, "images")
+        self.traps_list = arcade.tilemap.process_layer(map, "traps", 1, "images")
 
