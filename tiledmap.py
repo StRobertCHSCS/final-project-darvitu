ff656040043d3e31cfd6f3675bd753b8621cfb33
daniel-wang04 <daniel.wang221@ycdsbk12.ca>
2020-01-13 12:57:49 -0500

    adding fireball

--- tiledmap.py
+++ tiledmap.py
@@ -5,6 +5,7 @@ class TiledMap(arcade.TiledMap):
     def __init__(self):
         super().__init__()
 
+
     def tutorial_world(self):
         map = arcade.tilemap.read_tmx("Maps/tutorial-world.tmx")
         self.ground_list = arcade.tilemap.process_layer(map, "ground", 1, "images")
