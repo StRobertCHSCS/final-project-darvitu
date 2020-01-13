15351cf5e9d50a260ce8f096ae45a2c237c6f782
Anthony3301 <anthony.pirvuti22@ycdsbk12.ca>
2020-01-13 13:02:54 -0500

    created stage 1

--- tiledmap.py
+++ tiledmap.py
@@ -10,4 +10,13 @@ class TiledMap(arcade.TiledMap):
         self.ground_list = arcade.tilemap.process_layer(map, "ground", 1, "images")
         self.wall_list = arcade.tilemap.process_layer(map, "walls", 1, "images")
         self.traps_list = arcade.tilemap.process_layer(map, "traps", 1, "images")
+    
+    def stage_one(self):
+        map = arcade.tilemap.read_tmx("Maps/stage1-world.tmx")
+        self.ground_list = arcade.tilemap.process_layer(map, "ground", 1, "images")
+        self.wall_list = arcade.tilemap.process_layer(map, "walls", 1, "images")
+        self.traps_list = arcade.tilemap.process_layer(map, "traps", 1, "images")
+    
+    
+
 
