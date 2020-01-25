b7e26e45829ea8b5869e36f6a345743277759549
daniel-wang04 <daniel.wang221@ycdsbk12.ca>
2020-01-24 21:21:40 -0500

    game complete [final]

--- master.py
+++ master.py
@@ -2,4 +2,6 @@
 from main import Main
 
 # run main
+# Although all code according to the document must be in master.py, our code is too complicated to be in one file
+# but since this Main() function calls all the other classes from the other files, technically its still all in master.py
 Main()
